f199ec66a5bb7522a01fdc50c005bd2cca5a6e19
yud1z <effendeyl@garena.co.id>
2020-08-30 16:34:57 +0700

    init

---
+++ start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo "must root"
+    exit
+fi
+
+cd /tmp
+wget https://github.com/neovim/neovim/releases/download/v0.4.4/nvim.appimage
+chmod -R 777 nvim.appimage
+cp nvim.appimage /usr/bin/nvim.appimage
+ln -s /usr/bin/nvim.appimage /usr/bin/nvim
+rm -rf /tmp/nvim.appimage
+echo "done with installing nvim"
+
+echo "starting to adding my configuration"
+rm -rf ~/.vim
+cd /tmp
+git clone https://github.com/yud1z/nvim_config.git
+cd nvim_config
+mv .vim ~/
+ln -s ~/.vim ~/.config
+mkdir ~/vimtmp
+apt install exuberant-ctags silversearcher-ag
