a09ccedddca171fa702c85e5fdc7c0f644719590
yewewdod <87128564+yewewdod@users.noreply.github.com>
2021-08-04 10:20:29 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,10 +7,11 @@ fi
 
 cd /tmp
 wget https://github.com/neovim/neovim/releases/download/v0.4.4/nvim.appimage
-chmod -R 777 nvim.appimage
-cp nvim.appimage /usr/bin/nvim.appimage
-ln -s /usr/bin/nvim.appimage /usr/bin/nvim
-rm -rf /tmp/nvim.appimage
+chmod -R 777 nvim.appimage 
+./nvim.appimage --appimage-extract
+mv squashfs-root nvim_bin
+mv nvim_bin /
+ln -s /nvim_bin/AppRun /usr/bin/nvim
 echo "done with installing nvim"
 
 echo "starting to adding my configuration"
